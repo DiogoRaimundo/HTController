c186135fcf02591e42652746db3b43cf9a544e8c
Diogo Raimundo <diogo.raimundo@vortal.biz>
2022-11-13 22:02:31 +0000

    v1 - LCD timeout added + small refactor

--- basic_solution_ino/basic_solution_ino.ino
+++ basic_solution_ino/basic_solution_ino.ino
@@ -44,17 +44,50 @@ IRData *irData;
 boolean isDeviceOn = false;
 boolean prevIsDeviceOn = false;
 
-void configure_pins()
+int encoderPosition = 0;
+bool button1State = LOW;
+int dutyCycle = 50;
+
+unsigned long lastActivityTimestamp = 0;
+const unsigned long displayTimeout = 15 * 1000;
+bool displayState = HIGH;
+
+#pragma region LCD Utils
+void turnOnLcd()
+{
+  displayState = HIGH;
+  lcd->display();
+  digitalWrite(LCD_BACKLIGHT_PIN, HIGH);
+}
+
+void turnOffLcd()
+{
+  displayState = LOW;
+  lcd->noDisplay();
+  digitalWrite(LCD_BACKLIGHT_PIN, LOW);
+}
+
+void setActivityTimestamp()
+{
+  lastActivityTimestamp = millis();
+}
+#pragma endregion
+
+#pragma region Initialization
+
+void configurePins()
 {
   // pinMode(FTDI_RX_PIN, OUTPUT); NOT NEEDED
   // pinMode(FTDI_TX_PIN, OUTPUT); NOT NEEDED
-  // pinMode(IR_RECEIVER_PIN, INPUT); NOT NEEDED - IR
-  // pinMode(IR_EMITTER_PIN, INPUT); NOT NEEDED - IR
   pinMode(DEVICE_LED_PIN, OUTPUT);
-  // pinMode(FAN_CONTROL_PIN, OUTPUT); NOT NEEDED - FAN
-  // pinMode(FAN_SENSOR_PIN, INPUT); NOT NEEDED - FAN
-  // pinMode(ENCODER_PIN_1, INPUT); NOT NEEDED - ENCODER
-  // pinMode(ENCODER_PIN_2, INPUT); NOT NEEDED - ENCODER
+  pinMode(BUTTON_1_PIN, INPUT);
+  pinMode(BUTTON_2_PIN, INPUT);
+  // pinMode(CURRENT_SENSOR_PIN, INPUT); ANALOG INPUT
+  // pinMode(TEMPERATURE_SENSOR_PIN, INPUT); ANALOG INPUT
+}
+
+void initLcd()
+{
   // pinMode(LCD_RS_PIN, INPUT); NOT NEEDED - LCD
   // pinMode(LCD_EN_PIN, INPUT); NOT NEEDED - LCD
   pinMode(LCD_BACKLIGHT_PIN, OUTPUT);
@@ -63,32 +96,33 @@ void configure_pins()
 
   // pinMode(LCD_D6_PIN, INPUT); NOT NEEDED - LCD
   // pinMode(LCD_D7_PIN, INPUT);  NOT NEEDED - LCD
-  pinMode(BUTTON_1_PIN, INPUT);
-  pinMode(BUTTON_2_PIN, INPUT);
-  // pinMode(CURRENT_SENSOR_PIN, INPUT); ANALOG INPUT
-  // pinMode(TEMPERATURE_SENSOR_PIN, INPUT); ANALOG INPUT
-}
 
-void init_lcd()
-{
   lcd = new LiquidCrystal(LCD_RS_PIN, LCD_EN_PIN, LCD_D4_PIN, LCD_D5_PIN, LCD_D6_PIN, LCD_D7_PIN);
 
   lcd->begin(16, 2);
-  digitalWrite(LCD_BACKLIGHT_PIN, HIGH);
+
+  turnOnLcd();
+  setActivityTimestamp();
 }
 
-void init_ir_comms()
+void initIr()
 {
+  // pinMode(IR_RECEIVER_PIN, INPUT); NOT NEEDED - IR
+  // pinMode(IR_EMITTER_PIN, INPUT); NOT NEEDED - IR
+
   IrReceiver.begin(IR_RECEIVER_PIN, DISABLE_LED_FEEDBACK);
   IrSender.begin(IR_EMITTER_PIN, DISABLE_LED_FEEDBACK);
 }
 
-void init_encoder()
+void initEncoder()
 {
+  // pinMode(ENCODER_PIN_1, INPUT); NOT NEEDED - ENCODER
+  // pinMode(ENCODER_PIN_2, INPUT); NOT NEEDED - ENCODER
+
   encoder = new RotaryEncoder(ENCODER_PIN_1, ENCODER_PIN_2, RotaryEncoder::LatchMode::TWO03);
 }
 
-void init_fan()
+void initFan()
 {
   //  Change D5 & D6 PWM frenquency to 62.5kHz
   // TCCR0B &= B11111000 | B00000011; // D5 & D6 DEFAULT   (00976.56 Hz)
@@ -98,20 +132,27 @@ void init_fan()
   // TCCR1B &= B11111000 | B00000100; // D9 & D10 DEFAULT  (00490.20 Hz)
   // TCCR1B &= B11111000 | B00000001; // D9 & D10 MAX FREQ (31372.55 Hz) <- BEST OPTION
 
+  // pinMode(FAN_CONTROL_PIN, OUTPUT); // NOT NEEDED - FAN
+  // pinMode(FAN_SENSOR_PIN, INPUT);   // NOT NEEDED - FAN
+
   fan = new FanController(FAN_SENSOR_PIN, FAN_SENSOR_THRESHOLD, FAN_CONTROL_PIN);
 }
 
 void setup()
 {
-  configure_pins();
+  configurePins();
 
-  init_lcd();
-  init_ir_comms();
-  init_encoder();
-  init_fan();
+  initLcd();
+  initIr();
+  initEncoder();
+  initFan();
 }
 
-void ir_loop()
+#pragma endregion
+
+#pragma region Loop
+
+void irLoop()
 {
   if (IrReceiver.available())
   {
@@ -119,6 +160,8 @@ void ir_loop()
 
     if (irData->address == IR_ON_OFF_ADDRESS)
     {
+      setActivityTimestamp();
+
       if (irData->command == IR_ON_OFF_CMD)
       {
         isDeviceOn = !isDeviceOn;
@@ -131,60 +174,122 @@ void ir_loop()
   }
 }
 
-int encoderPosition = 0;
-bool button1LastState = LOW;
-int dutyCycle = 50;
+void encoderLoop()
+{
+  encoder->tick();
+
+  int newEncoderPosition = encoder->getPosition();
+  if (encoderPosition == newEncoderPosition)
+  {
+    return;
+  }
+
+  setActivityTimestamp();
+  encoderPosition = newEncoderPosition;
+
+  if (encoder->getDirection() == RotaryEncoder::Direction::COUNTERCLOCKWISE)
+  {
+    if (++dutyCycle > 100)
+    {
+      dutyCycle = 100;
+    }
+  }
+  else if (--dutyCycle < 0)
+  {
+    dutyCycle = 0;
+  }
+
+  if (isDeviceOn)
+  {
+    fan->setDutyCycle(dutyCycle);
+    // analogWrite(FAN_CONTROL_PIN, 2.55 * dutyCycle);
+  }
+}
+
+void button1Loop()
+{
+  bool currState = digitalRead(BUTTON_1_PIN);
+  if (currState == button1State)
+  {
+    return;
+  }
+
+  setActivityTimestamp();
+  button1State = currState;
+
+  if (!currState)
+  {
+    return;
+  }
+
+  isDeviceOn = !isDeviceOn;
+}
+
+void handleDeviceStateChange()
+{
+  if (prevIsDeviceOn == isDeviceOn)
+  {
+    return;
+  }
+
+  prevIsDeviceOn = isDeviceOn;
+
+  digitalWrite(DEVICE_LED_PIN, isDeviceOn);
+
+  if (isDeviceOn)
+  {
+    fan->setDutyCycle(dutyCycle);
+    // analogWrite(FAN_CONTROL_PIN, 2.55 * dutyCycle);
+  }
+  else
+  {
+    fan->setDutyCycle(0);
+    // digitalWrite(FAN_CONTROL_PIN, LOW);
+  }
+}
+
+void updateLcd()
+{
+  unsigned long displayTimespan = millis() - lastActivityTimestamp;
+  if (displayState)
+  {
+    if (displayTimespan >= displayTimeout)
+    {
+      turnOffLcd();
+    }
+  }
+  else
+  {
+    if (displayTimespan < displayTimeout)
+    {
+      turnOnLcd();
+    }
+    else
+    {
+      return;
+    }
+  }
+
+  lcd->setCursor(0, 0);
+  // lcd->print(String(millis() / 1000, DEC) + " | " + (isDeviceOn ? "ON " : "OFF"));
+  lcd->print(isDeviceOn ? "ON " : "OFF");
+
+  lcd->setCursor(0, 1);
+  // lcd->print(String(dutyCycle, DEC) + " | " + String(fan->getSpeed(), DEC));
+  lcd->print(String(dutyCycle, DEC) + "% ");
+}
 
 void loop()
 {
-  ir_loop();
+  irLoop();
 
-  encoder->tick();
-  int newEncoderPosition = encoder->getPosition();
-  if (encoderPosition != newEncoderPosition)
-  {
-    encoderPosition = newEncoderPosition;
+  encoderLoop();
 
-    if (encoder->getDirection() == RotaryEncoder::Direction::CLOCKWISE)
-    {
-      if (++dutyCycle > 100)
-      {
-        dutyCycle = 100;
-      }
-    }
-    else if (--dutyCycle < 0)
-    {
-      dutyCycle = 0;
-    }
+  button1Loop();
 
-    fan->setDutyCycle(isDeviceOn ? dutyCycle : 0);
-  }
+  handleDeviceStateChange();
 
-  if (prevIsDeviceOn != isDeviceOn)
-  {
-    prevIsDeviceOn = isDeviceOn;
-    fan->setDutyCycle(isDeviceOn ? dutyCycle : 0);
-  }
-
-  if (digitalRead(BUTTON_1_PIN) != button1LastState)
-  {
-    button1LastState = !button1LastState;
-    isDeviceOn = !isDeviceOn;
-  }
-
-  if (digitalRead(BUTTON_2_PIN))
-  {
-    digitalWrite(FAN_CONTROL_PIN, 0);
-  }
-
-  // digitalWrite(DEVICE_LED_PIN, digitalRead(BUTTON_1_PIN));
-  // digitalWrite(IR_EMITTER_PIN, digitalRead(BUTTON_2_PIN));
-
-  lcd->setCursor(0, 0);
-  lcd->print(String(millis() / 1000, DEC) + " | " + (isDeviceOn ? "ON " : "OFF"));
-
-  lcd->setCursor(0, 1);
-  lcd->print(String(dutyCycle, DEC) + " | " + String(fan->getSpeed(), DEC));
-
-  digitalWrite(DEVICE_LED_PIN, isDeviceOn);
+  updateLcd();
 }
+
+#pragma endregion
